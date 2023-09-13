992fc6c819b367b525791d5bb736dff62e86bf40
Tharuneshwar <tharuneshwarv@gmail.com>
2023-09-13 21:48:31 +0530

    Banner - Logo and avatar position

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className='navbar'>
       <div className='navbar__content'>
-        <img id='navbar__logo' src='https://www.pngplay.com/wp-content/uploads/7/Netflix-Logo-Background-PNG-Image.png' alt='logo' />
+        <img id='navbar__logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt='logo' />
         <img id='navbar__avatar' src='https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png' alt='avatar' />
       </div>
     </div>
