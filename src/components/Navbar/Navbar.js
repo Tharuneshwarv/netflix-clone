78b6a66eb33a0e06b69f080321e7379e77b8ddd3
Tharuneshwar <tharuneshwarv@gmail.com>
2023-09-16 20:35:26 +0530

    Add transition for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './navbar.css'
 
 function Navbar() {
+  let [bgColor, setBgColor] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavbarOnScroll());
+    return (
+      window.removeEventListener('scroll', changeNavbarOnScroll())
+    );
+  }, []);
+
+  function changeNavbarOnScroll() {
+    console.log('scrolled ',  window.scrollY);
+    if(window.scrollY > 100)
+      setBgColor(true)
+    else
+      setBgColor(false);
+  }
+
   return (
-    <div className='navbar'>
+    <div className={`navbar ${bgColor ? 'navbar__black': ''}`}>
       <div className='navbar__content'>
         <img id='navbar__logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt='logo' />
         <img id='navbar__avatar' src='https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png' alt='avatar' />
