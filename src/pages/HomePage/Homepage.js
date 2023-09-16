e7a307d625a05757bd43ec76804a6c9bddfd23db
Tharuneshwar <tharuneshwarv@gmail.com>
2023-09-16 08:20:38 +0530

    Navbar CSS Updated

--- src/pages/HomePage/Homepage.js
+++ src/pages/HomePage/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './Homepage.css'
+import Banner from '../../components/Banner/Banner'
 function Homepage() {
   return (
     <div className='homepage'>
         <Navbar />
+        <Banner />
     </div>
   )
 }
