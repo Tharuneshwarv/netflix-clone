3738644a57deabe6601b157ab1ecce5b53ce14b5
Tharuneshwar <tharuneshwarv@gmail.com>
2023-09-17 23:23:00 +0530

    Add suggestion rows

---
+++ src/components/Row/Row.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { getMovie } from '../../service/requests';
+import './Row.css'
+
+function Row({title, url }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        getMovie(url).then(res => {
+            setMovies(res);
+        })
+    }, [url])
+    return (
+        <div className='row'>
+            <h2 className='row__title'>{title}</h2>
+            <div className='row__posters'>
+                    {movies.map(movie => (movie?.backdrop_path && 
+                        <img id={`${movie.id}`} className='row__poster' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title || movie.original_name}/>
+                    ))}
+            </div>
+        </div>  
+    )
+}
+
+export default Row
