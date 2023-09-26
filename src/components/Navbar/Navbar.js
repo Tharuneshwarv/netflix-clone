98298d8f07b0b25726580eeda6e1560df280a17c
Tharuneshwar V <tharuneshwarv@gmail.com>
2023-09-26 22:27:54 +0530

    Add signout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './navbar.css'
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/reducers/userReducer';
 
 function Navbar() {
   let [bgColor, setBgColor] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     window.addEventListener('scroll', changeNavbarOnScroll);
@@ -15,11 +22,20 @@ function Navbar() {
     window.scrollY > 100 ? setBgColor(true) : setBgColor(false);
   };
 
+  const signOutEvent = (event) => {
+    event.preventDefault();
+    signOut(auth).then(() => console.log("Signout Successful..."))
+    .catch((error) => alert("SignOut unsuccessful"));
+  }
+
   return (
     <div className={`navbar ${bgColor ? 'navbar__black': ''}`}>
       <div className='navbar__content'>
-        <img id='navbar__logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt='logo' />
-        <img id='navbar__avatar' src='https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png' alt='avatar' />
+        <img id='navbar__logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt='logo'  onClick={(e) => navigate("/")}/>
+        <div className='navbar__right'>
+          <button className='navbar__signout' onClick={(e) => signOutEvent(e)}>Sign Out</button>
+          <img id='navbar__avatar' src='https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png' alt='avatar' onClick={(e) => navigate("/profile")}/>
+        </div> 
       </div>
     </div>
   )
