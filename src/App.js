da0c308c9e19775e647321e91a458aad18d0d361
Tharuneshwar V <tharuneshwarv@gmail.com>
2023-09-26 22:27:39 +0530

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import { Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/HomePage/Homepage';
 import { useState, useEffect } from 'react';
 import LoginPage from './pages/LoginPage/LoginPage';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './redux/reducers/userReducer';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  useEffect(() => setUser(''), []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if(user) {
+        dispatch(login({
+          uid: user.uid,
+          email: user.email
+        }));
+      } else {
+        dispatch(logout()); 
+      } 
+    });
+    return unsubscribe;
+  }, [dispatch]);
 
-  
-    console.log(user);
   return (
     <div className="App"> 
-    { 
-      !user ? <LoginPage/>: <Homepage />
-    }
+      <Router >
+        { 
+          !user ? <LoginPage/>: (
+            <Routes>
+              <Route path='/' element={<Homepage/>} />
+              <Route path='/profile' element={<ProfilePage />} />
+            </Routes>
+            
+          )
+        }
+      </Router>
+    
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Login.css'
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { useDispatch } from 'react-redux';
+import { login, populateUser } from '../../redux/reducers/userReducer';
+
 function Login() {
-  return (
-    <div className='login'>
-        <h1>Log In</h1>
-        <form>
-            <input type='email' placeholder='Email'/>
-            <input type='password' placeholder='Password' />
-            <button type='submit'>Log In</button>
-        </form>
-        <h4>
-            <span className='login__newToNetflix'>New to Netflix? </span> 
-            <span className='login__link'> SignUp Now.</span>
-        </h4>
-    </div>
-  )
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const dispatch = useDispatch();
+
+    const register = (event) => {
+        event.preventDefault();
+        createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+        .then((userAuth) => dispatch(login({
+            uid: userAuth.user.uid,
+            email: userAuth.user.email
+        })))
+        .catch((e) => alert(e));
+    }
+
+    const logIn = (event) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+        .then((userAuth) => dispatch(login({
+            uid: userAuth.user.uid,
+            email: userAuth.user.email
+        })))
+        .catch((e) => alert(e));
+
+    }
+    return (
+        <div className='login'>
+            <h1>Log In</h1>
+            <form>
+                <input ref={emailRef} type='email' placeholder='Email'/>
+                <input ref={passwordRef} type='password' placeholder='Password' />
+                <button type='submit' onClick={(e) => logIn(e)}>Log In</button>
+            </form>
+            <h4>
+                <span className='login__newToNetflix'>New to Netflix? </span> 
+                <span className='login__link' onClick={(e) => register(e)}> SignUp Now.</span>
+            </h4>
+        </div>
+    )
 }
 
 export default Login
